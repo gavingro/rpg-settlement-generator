9c048ea607ae8b6f06282f3cfaa9b7976e952271
Gavin Grochowski <grochowskigavin@gmail.com>
2021-02-18 20:28:45 -0800

    Setup Unit Testing Framework

---
+++ test.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class FakeTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def first_test_suite(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
