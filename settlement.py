b74e8b9e8da7830245c8b02c67cdcb0c20420dc9
Gavin Grochowski <grochowskigavin@gmail.com>
2021-02-18 21:18:15 -0800

    Settlement Parent Class ( and Unit Testing )

---
+++ settlement.py
@@ -0,0 +1,33 @@
+class Settlement:
+    """
+    A parent Settlement class to interface with the generation of settlements.
+
+    Other Settlements will derive from this class.
+    """
+
+    def __init__(self):
+        self.template = self.generate_template()
+
+    # Generator scripts to ovverride in subclasses.
+    #
+    #
+
+    def generate_attributes(self):
+        """ Randomly generates settlement components for filling template."""
+        pass
+
+    def generate_template(self):
+        """ Randomly Generates a settlement template to fill with attributes."""
+        return "This is the base settlement generator."
+
+    def fill_template(self, template):
+        """ Replaces placeholders in template with their attributes"""
+        return template
+
+    def generate(self):
+        """Returns prepared template, all filled in."""
+        return self.fill_template(self.template)
+
+    # Common methods that multiple subclasses use.
+    #
+    #
--- test.py
+++ test.py
@@ -1,12 +1,33 @@
 import unittest
+from random import seed
+from settlement import Settlement
 
 
-class FakeTest(unittest.TestCase):
+class TestSettlement(unittest.TestCase):
     def setUp(self):
-        pass
+        seed(69)
+        self.test_settlement = Settlement()
 
-    def first_test_suite(self):
-        pass
+    def test_settlement_template(self):
+        self.assertEqual(
+            self.test_settlement.generate_template(),
+            "This is the base settlement generator.",
+        )
+        self.assertEqual(
+            self.test_settlement.template, "This is the base settlement generator.",
+        )
+
+    def test_settlement_attributes(self):
+        self.assertIsNone(self.test_settlement.generate_attributes())
+
+    def test_settlement_fill(self):
+        self.assertNotIn(
+            "{", self.test_settlement.fill_template(self.test_settlement.template)
+        )
+
+    def test_settlement_generator(self):
+        self.assertIsInstance(self.test_settlement.generate(), str)
+        print(self.test_settlement.generate())
 
 
 if __name__ == "__main__":
